d519a8d92ea305cc45524a84270f9992b9f4a677
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-08-16 01:03:43 +0400

    update signin header

--- src/components/auth/signIn/SignIn.tsx
+++ src/components/auth/signIn/SignIn.tsx
@@ -70,11 +70,11 @@ export const SignIn: React.FC<SignInProps> = ({ closeSignIn }) => {
     <section className="bg-gray-50 dark:bg-gray-800 transiton duration-300">
       {!isForgot && (
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <h1 className="text-xl my-2 font-bold leading-tight tracking-tight md:text-2xl text-gray-900 dark:text-white">
-            Sign in to your account
-          </h1>
           <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
             <div className="p-6 space-y-4 md:space-y-6 transiton duration-300 sm:p-8 dark:bg-gray-700 dark:rounded-md">
+              <h1 className="text-xl my-2 font-bold leading-tight tracking-tight md:text-2xl text-gray-900 dark:text-white">
+                Sign in to your account
+              </h1>
               <form
                 className="space-y-4 md:space-y-6"
                 onSubmit={submitSignInData}
